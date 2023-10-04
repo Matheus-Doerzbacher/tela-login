8e58ba381b2fda72bb60f74b7392b965b0589460
MathePLay <Matheus.doer@gmail.com>
2023-10-03 22:11:34 -0300

    finalizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,74 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
+"use client"
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    function adicionar(){
+        const body = document.querySelector('body');
+        body.classList.add("signup");
+    }
+    
+    function remover(){
+        const body = document.querySelector('body');
+        body.classList.remove("signup");
+    }
+
+    return (
+        <div className="container">
+            <span></span>
+            <span></span>
+            <span></span>
+
+            <form id="signinForm">
+                <h2>Login</h2>
+                
+                <div className="inputBox">
+                    <input type="text" placeholder="Username" />
+                </div>
+                
+                <div className="inputBox">
+                    <input type="password" placeholder="Password" />
+                </div>
+                
+                <div className="inputBox group">
+                    <a href="#">Forgot Password</a>
+                    <a href="#" id="signup" onClick={adicionar}>
+                        Signup
+                    </a>
+                </div>
+                
+                <div className="inputBox">
+                    <input type="submit" value="Sign in" />
+                </div>
+            </form>
+
+            <form id="signupForm">
+                <h2>Registration</h2>
+                
+                <div className="inputBox">
+                    <input type="text" placeholder="Username" />
+                </div>
+
+                <div className="inputBox">
+                    <input type="text" placeholder="Email Address" />
+                </div>
+                
+                <div className="inputBox">
+                    <input type="password" placeholder="Create Password" />
+                </div>
+
+                <div className="inputBox">
+                    <input type="password" placeholder="Confirm Password" />
+                </div>
+                
+                <div className="inputBox">
+                    <input type="submit" value="Register Account" />
+                </div>
+
+                <div className="inputBox group">
+                    <a href="#" id="login">
+                        Already Have an Account ?
+                        <b className="signin" onClick={remover}>Login</b>
+                    </a>
+                </div>
+            </form>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
 }
